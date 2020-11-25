40988a2052f9f17b7d8b74c9344a87012c0d7545
Burrypony <kiril.chernysh@gmail.com>
2020-11-25 13:12:05 +0200

    debug account balance v4

--- sidebar/account-balance/beauty_bode_sidebar-account_balance.js
+++ sidebar/account-balance/beauty_bode_sidebar-account_balance.js
@@ -3,7 +3,7 @@ customHomepage.accountBalance=function(uuid,config){
   customHomepage.accountBalanceConfig=config
   pepperi.api.accounts.get({
      key: { UUID: uuid },
-     fields: [config.field, config.measure_unit],
+     fields: [config.field],
      responseCallback:"customHomepage.accountBalanceCallBack"
 });
 }
@@ -15,7 +15,7 @@ customHomepage.accountBalanceCallBack=function(data){
   document.getElementById("account_balance").innerHTML = `                  
   <div>
   <p class="dimmed">${customHomepage.accountBalanceConfig.text}</p>
-  <p class="title-2-sm "><b id='balance'>${data.object[customHomepage.accountBalanceConfig.field]}</b> ${data.object[customHomepage.accountBalanceConfig.measure_unit]}</p>
+  <p class="title-2-sm "><b id='balance'>${data.object[customHomepage.accountBalanceConfig.field]}</b> ${customHomepage.accountBalanceConfig.measure_unit}</p>
   </div>` + (customHomepage.accountBalanceConfig.svg ? `<img src="${customHomepage.accountBalanceConfig.svg}" alt="Go to Account Balance icon">` : '')
   document.getElementById("account_balance").classList.add("card", "sidebar-gap")  
 }
