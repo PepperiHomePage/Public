0db5c4ec1f0b7ad5726f4884d114623cf28cb1a4
Burrypony <kiril.chernysh@gmail.com>
2020-11-27 16:07:41 +0200

    fix store selector v2

--- sidebar/account-dropdown/beauty_body_sidebar_store-selector.js
+++ sidebar/account-dropdown/beauty_body_sidebar_store-selector.js
@@ -46,11 +46,12 @@ customHomepage.buildAccountsDropDown = function (thisAccounts, callback) {
     document.getElementById("store-selector").classList.add("sidebar-box")
     document.getElementById("store-selector").classList.add("sidebar-gap")
   }
+  var x = element.Name + `(${element.ExternalID})`;
   accounts.forEach((element) => {
     if ((customHomepage.getSessionStorage("accountUUID") && customHomepage.getSessionStorage("accountUUID") != '' && element.UUID == customHomepage.getSessionStorage("accountUUID"))) {
       html += `<label class="title-1-xs sidebar-gap" for="order-for">Order for:</label>
       <div class="custom-input-dropdown" onclick="customHomepage.openStoreSelect()">
-        <p role="label" id="selected-account">Select a store</p>
+        <p role="label" id="selected-account">${x}</p>
         <ul class="dropdown-content-fit" id="select-menu" role="select">
         <li class="active-dropdown-item" onclick="customHomepage.setActiveDropdown('${element.UUID}','${element.Name}(${element.ExternalID})'); customHomepage.findTransactionForSelectedAccount('${element.UUID}')" id="${element.UUID}">${element.Name}(${element.ExternalID})</li>
         </ul>
@@ -58,9 +59,8 @@ customHomepage.buildAccountsDropDown = function (thisAccounts, callback) {
             <path fill-rule="evenodd"
                 d="M5.80032148,9.28674354 L11.2542824,13.2027583 C11.6661309,13.585083 12.3338691,13.585083 12.7457176,13.2027583 L18.1996785,9.28674354 C18.611527,8.90441882 19.2792652,8.90441882 19.6911137,9.28674354 C20.1029621,9.66906826 20.1029621,10.2889391 19.6911137,10.6712638 L13.4914351,16.4265129 C12.6677383,17.1911624 11.3322617,17.1911624 10.5085649,16.4265129 L4.30888633,10.6712638 C3.89703789,10.2889391 3.89703789,9.66906826 4.30888633,9.28674354 C4.72073478,8.90441882 5.38847303,8.90441882 5.80032148,9.28674354 Z" />
         </svg>
-      </div>
-                  `;
-      document.getElementById("selected-account").innerHTML = element.Name + `(${element.ExternalID})`
+      </div>`;
+      ddElement.innerHTML = html;
       customHomepage.setSessionStorage("accountUUID", element.UUID);
     } else
       html += `<label class="title-1-xs sidebar-gap" for="order-for">Order for:</label>
