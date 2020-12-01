d092411049ec6105aa03ddacdc828ef32371f9a6
Burrypony <kiril.chernysh@gmail.com>
2020-12-01 11:44:21 +0200

    fix right menu on header

--- rightMenu/rightMenu.js
+++ rightMenu/rightMenu.js
@@ -5,8 +5,7 @@ customHeader.RightMenu = function(RightMenu){
       rightSideHtmlStr += `<button class="button-weak hidden-on-web"onclick="${customHeader.handleAction(item)}">${item.title}${item.icon ? item.icon : ''}</button>`;
       dropdownMenuMob += `<li class="active" onclick="${customHeader.handleAction(item)}"><p>${item.title}</p></li>`
     }
-    let rightAddMenu = document.getElementById('right_additional_menu').innerHTML;
-    document.getElementById('right_additional_menu').innerHTML = rightSideHtmlStr + rightAddMenu;
+    document.getElementById('right_additional_menu').innerHTML = rightSideHtmlStr;
     if (document.getElementById('menuDropdown')) {
       document.getElementById('menuDropdown').innerHTML += `<ul>${dropdownMenuMob}</ul>`;
     }
