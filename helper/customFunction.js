45ec0846c1bd725fb10c0b6a448ab1357650506c
Burrypony <kiril.chernysh@gmail.com>
2020-12-10 15:35:09 +0200

    fixed burder for header V2

--- helper/customFunction.js
+++ helper/customFunction.js
@@ -168,3 +168,19 @@ customFunction.createNewActivity = function (in_transactionName, deeplink, custo
       $('#myDropdown').removeClass('show');
     });
   };
+  customFunction.openCloseMenu = function () {
+    const over = document.getElementById("overlay");
+    const e = document.getElementById("sidebar-sm");
+    const btn = document.getElementById("btn");
+    if (e.style.display == "block") {
+      e.style.display = "none";
+      over.style.display = "none";
+      btn.innerText = "Open Menu";
+    } else {
+      over.style.display = "block";
+      e.style.display = "block";
+      btn.innerText = "Close Menu";
+      $('#sidebar-sm').attr('tabindex', '-1');
+      $('#sidebar-sm').focus()
+    }
+  };
--- jsListeners/beauty_demo_jsListeners.js
+++ jsListeners/beauty_demo_jsListeners.js
@@ -1,19 +1,4 @@
 customFunction.openStoreSelect = function () {
     document.getElementById('select-menu').classList.toggle('show')
   }
-  customFunction.openCloseMenu = function () {
-    const over = document.getElementById("overlay");
-    const e = document.getElementById("sidebar-sm");
-    const btn = document.getElementById("btn");
-    if (e.style.display == "block") {
-      e.style.display = "none";
-      over.style.display = "none";
-      btn.innerText = "Open Menu";
-    } else {
-      over.style.display = "block";
-      e.style.display = "block";
-      btn.innerText = "Close Menu";
-      $('#sidebar-sm').attr('tabindex', '-1');
-      $('#sidebar-sm').focus()
-    }
-  };
+  
