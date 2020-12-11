8e17989e6efcff81bc0ee51f12dd8df2fe487dc6
BurryPony <kiril.chernysh@gmail.com>
2020-12-11 10:25:08 +0200

    fix carousel v2

--- carousal/beauty_body_carousel.js
+++ carousal/beauty_body_carousel.js
@@ -63,7 +63,7 @@ customFunction.playerClick = function () {
     }
 };
 customFunction.switchSlide = function (isCurrent, next = true) {
-    clearTimeout(customFunction.slideSwitchTimeoutKeeper);
+    clearTimeout(this.switcher);
     let htmlStr = "";
 
     let indicatorsStr = "";
@@ -71,34 +71,24 @@ customFunction.switchSlide = function (isCurrent, next = true) {
     var idx;
     var value;
 
-    idx = +sessionStorage.getItem("savedIDX") < customFunction.CaruselData.length ?
-        +sessionStorage.getItem("savedIDX") :
-        0;
+    idx =
+      +sessionStorage.getItem("savedIDX") < CaruselData.length
+        ? +sessionStorage.getItem("savedIDX")
+        : 0;
 
-    value = customFunction.CaruselData[idx];
-    if (next) {
-        customFunction.setSessionStorage(
-            "savedIDX",
-            +sessionStorage.getItem("savedIDX") + 1 < customFunction.CaruselData.length ?
-            +sessionStorage.getItem("savedIDX") + 1 :
-            0
-        );
-    } else {
-        customFunction.setSessionStorage(
-            "savedIDX",
-            +sessionStorage.getItem("savedIDX") - 1 > 0 ?
-            +sessionStorage.getItem("savedIDX") - 1 :
-            3
-        );
+    value = CaruselData[idx];
+    customHomepage.setSessionStorage(
+      "savedIDX",
+      +sessionStorage.getItem("savedIDX") + 1 < CaruselData.length
+        ? +sessionStorage.getItem("savedIDX") + 1
+        : 0
+    );
 
-    }
-
-    htmlStr += `<div id="carousel" class="carousel"> 
-    <div id="slides" class="slides"><div class="slide"  data-state="active"
+    htmlStr += `<div class="slide"  data-state="active"
     style="background-image: url('${value.imageURL}')">
     <div class="gard-overlay">
         <div class="slide-text">
-            <button id="shop_now" onclick="customFunction.setUUIDandNav(null,null,'${value.deepLink}', 'customHomepage')" >${value.buttonText}</button>
+            <button id="shop_now" onclick="customHomepage.setUUIDandNav('${value.deepLink}',true)" >${value.buttonText}</button>
             <p class="title">${value.title}</p>
             <p class="desc">${value.description}</p>
         </div>
@@ -106,32 +96,49 @@ customFunction.switchSlide = function (isCurrent, next = true) {
             <div id="indicators" class="indicators">
                 
             </div>
-            <button onclick="customFunction.playerClick();" class="pause" id="player">
+            <button onclick="customHomepage.playerClick();" class="pause" id="player">
             </button>
         </div>
     </div>
-</div></div>
-</div>`
-    if (document.getElementById("carousal-content")) {
-        document.getElementById("carousal-content").innerHTML = htmlStr;
-        for (const [idx1, value] of customFunction.CaruselData.entries()) {
-            indicatorsStr +=
-                idx1 == idx ?
-                `<div class="radio-box">
-           <input type="radio" name="indicator" data-slide="${idx1}" data-time="${value.time}"  data-state="active" onclick="customFunction.switchSlide(true)" checked="checked">
-           <span class="radio-dot" data-slide="${idx1}" data-time="${value.time}"  data-state="active"  onclick="customFunction.switchSlide(true)"></span>
-           </div>` :
-                `<div class="radio-box">
-           <input type="radio" name="indicator" data-slide="${idx1}" data-time="${value.time}" onclick="customFunction.switchSlide(true)">
-           <span class="radio-dot" data-slide="${idx1}" data-time="${value.time}"  data-state="active"  onclick="customFunction.switchSlide(true)"></span>
+</div>`;
+    if (document.getElementById("slides")) {
+      document.getElementById("slides").innerHTML = htmlStr;
+
+      /* htmlStr += `<div class="slide" onclick="customHomepage.setUUIDandNav('${value.deepLink}',true)"  data-state="active" style="background-image: url('${value.imageURL}'), linear-gradient(to right, rgba(117, 117, 117, 0.7) 10%, rgba(163, 163, 163, .2))"></div>
+                           <div class="slide-text">
+                           <button id="shop_now" onclick="customHomepage.setUUIDandNav('/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.MainCategory%5C%22,%5C%22Value%5C%22:%5C%22Bakery%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%224cb18aba-1986-43a0-a5d1-f53433c6a589%5C%5C%5C%22%7D%5C%22%7D%22&ViewType=%7B%22Key%22:%22OrderCenterView3%22,%22Value%22:%22Medium%22%7D&TopPadding=0&SearchAll=false')"></button>
+                           <p class="title">${value.title}</p>
+                           <p class="desc">${value.description}</p>
+                       </div>`;*/
+
+      for (const [idx1, value] of CaruselData.entries()) {
+        indicatorsStr +=
+          /*idx1 === 0
+            ? `<input class="indicator" name="indicator" data-slide="${idx1}" data-time="${value.time}"  data-state="active" onclick="customHomepage.setSessionStorage('savedIDX', this.getAttribute('data-slide'));customHomepage.switchSlide(true)" checked type="radio"/>`
+            : `<input class="indicator" name="indicator" data-slide="${idx1}" data-time="${value.time}" onclick="customHomepage.setSessionStorage('savedIDX', this.getAttribute('data-slide'));customHomepage.switchSlide(true)" type="radio"/>`;
+          */
+          idx1 == idx
+            ? `<div class="radio-box">
+           <input type="radio" name="indicator" data-slide="${idx1}" data-time="${value.time}"  data-state="active" onclick="customHomepage.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customHomepage.switchSlide(true)" checked="checked">
+           <span class="radio-dot" data-slide="${idx1}" data-time="${value.time}"  data-state="active"  onclick="customHomepage.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customHomepage.switchSlide(true)"></span>
            </div>`
-        }
-        document.getElementById("indicators").innerHTML = indicatorsStr;
-        document.querySelectorAll(".slide-text")[0].style.opacity = 1;
-        customFunction.slideLifetyme = value.time;
-        customFunction.slideSwitchTimeoutKeeper = setTimeout(function () {
-            customFunction.switchSlide();
-        }, customFunction.slideLifetyme);
+            : `<div class="radio-box">
+           <input type="radio" name="indicator" data-slide="${idx1}" data-time="${value.time}" onclick="customHomepage.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customHomepage.switchSlide(true)">
+           <span class="radio-dot" data-slide="${idx1}" data-time="${value.time}"  data-state="active"  onclick="customHomepage.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customHomepage.switchSlide(true)"></span>
+           </div>`;
+      }
+      document.getElementById("indicators").innerHTML = indicatorsStr;
+      document.querySelectorAll(".slide-text")[0].style.opacity = 1;
+      var carousel = document.getElementById("carousel");
+      if (carousel) {
+        this.slides = carousel.querySelectorAll(".slide");
+        this.slideDesc = carousel.querySelectorAll(".slide-text");
+        this.indicators = carousel.querySelectorAll(".indicator");
+      }
+      this.speed = value.time;
+      this.switcher = setTimeout(function () {
+        customHomepage.switchSlide();
+      }, this.speed);
     }
 };
 customFunction.swipeListener = function () {
