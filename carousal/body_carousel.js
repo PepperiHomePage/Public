5e4487cca2499b359c5466c61c65702079e7c9a2
Burrypony <kiril.chernysh@gmail.com>
2020-12-21 11:04:43 +0200

    added clickable carousel

--- carousal/body_carousel.js
+++ carousal/body_carousel.js
@@ -9,7 +9,7 @@ customFunction.carousel = function (slideid, CaruselData) {
     var value = customFunction.CaruselData[idx];
 
     htmlStr += ` <div id="carousel" class="carousel"> 
-    <div id="slides" class="slides"><div class="slide" data-state="active"
+    <div id="slides" class="slides"  onclick="customFunction.setUUIDandNav(null,null,'/Transactions/scope_items/{{UUID}}','customHomepage')"><div class="slide" data-state="active"
     style="background-image: url('${value.imageURL}')">
     <div class="gard-overlay">
         <div class="slide-text">
@@ -21,7 +21,7 @@ customFunction.carousel = function (slideid, CaruselData) {
             <div id="indicators" class="indicators">
                 
             </div>
-            <button onclick="customFunction.playerClick();" class="pause" id="player">
+            <button onclick="event.stopImmediatePropagation();customFunction.playerClick();" class="pause" id="player">
             </button>
         </div>
     </div>
@@ -33,12 +33,12 @@ customFunction.carousel = function (slideid, CaruselData) {
         indicatorsStr +=
             idx1 == idx ?
             `<div class="radio-box">
-           <input type="radio" name="indicator" data-slide="${idx1}" data-time="${value.time}"  data-state="active" onclick="customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customFunction.switchSlide(true)" checked="checked">
-           <span class="radio-dot" data-slide="${idx1}" data-time="${value.time}"  data-state="active"  onclick="customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide'));  customFunction.switchSlide(true)" ></span>
+           <input type="radio" name="indicator" data-slide="${idx1}" data-time="${value.time}"  data-state="active" onclick="event.stopImmediatePropagation();customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customFunction.switchSlide(true)" checked="checked">
+           <span class="radio-dot" data-slide="${idx1}" data-time="${value.time}"  data-state="active"  onclick="event.stopImmediatePropagation();customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide'));  customFunction.switchSlide(true)" ></span>
            </div>` :
             `<div class="radio-box">
-           <input type="radio" name="indicator" data-slide="${idx1}" data-time="${value.time}" onclick="customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide'));  customFunction.switchSlide(true)">
-           <span class="radio-dot" data-slide="${idx1}" data-time="${value.time}"  data-state="active"  onclick="customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide'));  customFunction.switchSlide(true)"></span>
+           <input type="radio" name="indicator" data-slide="${idx1}" data-time="${value.time}" onclick="event.stopImmediatePropagation();customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide'));  customFunction.switchSlide(true)">
+           <span class="radio-dot" data-slide="${idx1}" data-time="${value.time}"  data-state="active"  onclick="event.stopImmediatePropagation();customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide'));  customFunction.switchSlide(true)"></span>
            </div>`
     }
 
@@ -87,7 +87,7 @@ customFunction.switchSlide = function (isCurrent, next = true) {
     style="background-image: url('${value.imageURL}')">
     <div class="gard-overlay">
         <div class="slide-text">
-            <button id="shop_now" onclick="customFunction.setUUIDandNav('${value.deepLink}',true)" >${value.buttonText}</button>
+            <button id="shop_now" onclick="customFunction.setUUIDandNav(null,null,'${value.deepLink}','customHomepage')" >${value.buttonText}</button>
             <p class="title">${value.title}</p>
             <p class="desc">${value.description}</p>
         </div>
@@ -95,35 +95,23 @@ customFunction.switchSlide = function (isCurrent, next = true) {
             <div id="indicators" class="indicators">
                 
             </div>
-            <button onclick="customFunction.playerClick();" class="pause" id="player">
+            <button onclick="event.stopImmediatePropagation();customFunction.playerClick();" class="pause" id="player">
             </button>
         </div>
     </div>
 </div>`;
     if (document.getElementById("slides")) {
         document.getElementById("slides").innerHTML = htmlStr;
-
-        /* htmlStr += `<div class="slide" onclick="customFunction.setUUIDandNav('${value.deepLink}',true)"  data-state="active" style="background-image: url('${value.imageURL}'), linear-gradient(to right, rgba(117, 117, 117, 0.7) 10%, rgba(163, 163, 163, .2))"></div>
-                             <div class="slide-text">
-                             <button id="shop_now" onclick="customFunction.setUUIDandNav('/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.MainCategory%5C%22,%5C%22Value%5C%22:%5C%22Bakery%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%224cb18aba-1986-43a0-a5d1-f53433c6a589%5C%5C%5C%22%7D%5C%22%7D%22&ViewType=%7B%22Key%22:%22OrderCenterView3%22,%22Value%22:%22Medium%22%7D&TopPadding=0&SearchAll=false')"></button>
-                             <p class="title">${value.title}</p>
-                             <p class="desc">${value.description}</p>
-                         </div>`;*/
-
         for (const [idx1, value] of CaruselData.entries()) {
             indicatorsStr +=
-                /*idx1 === 0
-                  ? `<input class="indicator" name="indicator" data-slide="${idx1}" data-time="${value.time}"  data-state="active" onclick="customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide'));customFunction.switchSlide(true)" checked type="radio"/>`
-                  : `<input class="indicator" name="indicator" data-slide="${idx1}" data-time="${value.time}" onclick="customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide'));customFunction.switchSlide(true)" type="radio"/>`;
-                */
                 idx1 == idx ?
                 `<div class="radio-box">
-           <input type="radio" name="indicator" data-slide="${idx1}" data-time="${value.time}"  data-state="active" onclick="customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customFunction.switchSlide(true)" checked="checked">
-           <span class="radio-dot" data-slide="${idx1}" data-time="${value.time}"  data-state="active"  onclick="customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customFunction.switchSlide(true)"></span>
+           <input type="radio" name="indicator" data-slide="${idx1}" data-time="${value.time}"  data-state="active" onclick="event.stopImmediatePropagation();customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customFunction.switchSlide(true)" checked="checked">
+           <span class="radio-dot" data-slide="${idx1}" data-time="${value.time}"  data-state="active"  onclick="event.stopImmediatePropagation();customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customFunction.switchSlide(true)"></span>
            </div>` :
                 `<div class="radio-box">
-           <input type="radio" name="indicator" data-slide="${idx1}" data-time="${value.time}" onclick="customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customFunction.switchSlide(true)">
-           <span class="radio-dot" data-slide="${idx1}" data-time="${value.time}"  data-state="active"  onclick="customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customFunction.switchSlide(true)"></span>
+           <input type="radio" name="indicator" data-slide="${idx1}" data-time="${value.time}" onclick="event.stopImmediatePropagation();customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customFunction.switchSlide(true)">
+           <span class="radio-dot" data-slide="${idx1}" data-time="${value.time}"  data-state="active"  onclick="event.stopImmediatePropagation();customFunction.setSessionStorage('savedIDX', this.getAttribute('data-slide')); customFunction.switchSlide(true)"></span>
            </div>`;
         }
         document.getElementById("indicators").innerHTML = indicatorsStr;
