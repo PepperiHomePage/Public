93b77c1409b92b1c1a2b0131bab8f718c0f4c6cf
Burrypony <kiril.chernysh@gmail.com>
2020-12-21 10:46:58 +0200

    rewrote account balance by Kostya example

--- sidebar/account-balance/body_sidebar-account_balance.js
+++ sidebar/account-balance/body_sidebar-account_balance.js
@@ -1,5 +1,6 @@
 customFunction.accountBalance=function(uuid,config,id){
   console.log("config ---> ",config);
+  if (config.field && config.field != '') {
   customFunction.accountBalanceConfig=config
   pepperi.api.accounts.get({
      key: { UUID: uuid },
@@ -7,11 +8,15 @@ customFunction.accountBalance=function(uuid,config,id){
      responseCallback:"customFunction.accountBalanceCallBack",
      requestID:id
 });
+  }else{
+    customFunction.accountBalanceCallBack(null, id)
+  }
 }
 
  
 
-customFunction.accountBalanceCallBack=function(data){
+customFunction.accountBalanceCallBack=function(data, id){
+  if (data) {
   console.log("data ---> ",data);
   document.getElementById(data.requestID).innerHTML = `                  
   <div>
@@ -20,5 +25,14 @@ customFunction.accountBalanceCallBack=function(data){
   </div>` + (customFunction.accountBalanceConfig.svg ? `<img src="${customFunction.accountBalanceConfig.svg}" alt="Go to Account Balance icon">` : '')
   document.getElementById(data.requestID).classList.add("card", "sidebar-gap")  
   document.getElementById(data.requestID).style.display = "flex"
+  }else{
+    document.getElementById(data.requestID).innerHTML = `                  
+  <div>
+  <p class="dimmed">${customFunction.accountBalanceConfig.text}</p>
+  <p class="title-2-sm "> ${customFunction.accountBalanceConfig.measure_unit}</p>
+  </div>` + (customFunction.accountBalanceConfig.svg ? `<img src="${customFunction.accountBalanceConfig.svg}" alt="Go to Account Balance icon">` : '')
+  document.getElementById(id).classList.add("card", "sidebar-gap")  
+  document.getElementById(id).style.display = "flex"
+  }
 }
 
