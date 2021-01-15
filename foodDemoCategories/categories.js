dd3a2b1c2510558206a7fd20c0ee6760bcf727f2
Burrypony <kiril.chernysh@gmail.com>
2021-01-15 10:36:17 +0200

    fix onclick food demo categories for all img

--- foodDemoCategories/categories.js
+++ foodDemoCategories/categories.js
@@ -8,29 +8,29 @@ customFunction.buildCategories = function (slideid) {
                     <div class="overlay">${left_top_img_left.title}</div>
                 </div>
                 <div class="item2"
-                onclick="customFunction.setUUIDandNav('null,null,${left_top_img_right.deepLink}')"
+                onclick="customFunction.setUUIDandNav(null,null,'${left_top_img_right.deepLink}','customHomepage')"
                 style="background-image: url(${left_top_img_right.image});">
                     <div class="overlay">${left_top_img_right.title}</div>
                 </div>
                 <div class="item3"
-                onclick="customFunction.setUUIDandNav('null,null,${left_bottom_img.deepLink}')"
+                onclick="customFunction.setUUIDandNav(null,null,'${left_bottom_img.deepLink}','customHomepage')"
                 style="background-image: url(${left_bottom_img.image});">
                     <div class="overlay">${left_bottom_img.title}</div>
                 </div>
               </div>
               <div class="box box2">
                  <div class="item4" 
-                 onclick="customFunction.setUUIDandNav(null,null,'${midle_top_left_img.deepLink}')"
+                 onclick="customFunction.setUUIDandNav(null,null,'${midle_top_left_img.deepLink}','customHomepage')"
                  style="background-image: url(${midle_top_left_img.image});">
                      <div class="overlay">${midle_top_left_img.title}</div>
                  </div>
                  <div class="item5"
-                 onclick="customFunction.setUUIDandNav('null,null,${midle_bottom_left_img.deepLink}')"
+                 onclick="customFunction.setUUIDandNav(null,null,'${midle_bottom_left_img.deepLink}','customHomepage')"
                   style="background-image: url(${midle_bottom_left_img.image});">
                    <div class="overlay">${midle_bottom_left_img.title}</div> 
                  </div>
                   <div class="item6"
-                  onclick="customFunction.setUUIDandNav('null,null,${midle_right_img.deepLink}')"
+                  onclick="customFunction.setUUIDandNav(null,null,'${midle_right_img.deepLink}','customHomepage')"
                   style="background-image: url(${midle_right_img.image});">
                   <div class="overlay">${midle_right_img.title}</div>
                   </div>
@@ -38,12 +38,12 @@ customFunction.buildCategories = function (slideid) {
   
               <div class="box3">
                   <div class="item7"
-                  onclick="customFunction.setUUIDandNav('null,null,${right_top_img.deepLink}')"
+                  onclick="customFunction.setUUIDandNav(null,null,'${right_top_img.deepLink}','customHomepage')"
                   style="background-image: url(${right_top_img.image});">
                       <div class="overlay">${right_top_img.title}</div>
                   </div>
                   <div class="item8"
-                  onclick="customFunction.setUUIDandNav('null,null,${right_bottom_img.deepLink}')"
+                  onclick="customFunction.setUUIDandNav(null,null,'${right_bottom_img.deepLink}','customHomepage')"
                   style="background-image: url(${right_bottom_img.image});">
                        <div class="overlay">${right_bottom_img.title}</div>
                   </div>
