b347b0510be14a8ec91d42be0954854c5d996e22
Alisa Skrypnyk <36660963+AlisaSkryp@users.noreply.github.com>
2021-02-02 15:33:46 +0300

    Update config_body.js

--- Beauty/config_body.js
+++ Beauty/config_body.js
@@ -2,7 +2,7 @@ var Transaction = 'B2B Order';
 var Catalog='ALL'
 var blocks_config = {
     'free_shipping': {
-        text: "Free shipping for orders over $",
+        text: "Free shipping for orders over 50$",
         field: "TSAFreeShipping",
         svg:"https://storage.pepperi.com/General/Icons/truck.svg"
     },
