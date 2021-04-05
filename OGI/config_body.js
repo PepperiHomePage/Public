51f4bbcc3da5ad9184c2580f1679a924c6c19384
unknown <kiril.chernysh@gmail.com>
2021-04-05 17:18:01 +0300

    added ogi

---
+++ OGI/config_body.js
@@ -0,0 +1,167 @@
+var Transaction = 'B2B Order';
+var Catalog='ALL'
+var blocks_config = {
+    'free_shipping': {
+        text: "Free shipping for orders over $",
+        field: "TSAFreeShipping",
+        svg:"https://storage.pepperi.com/General/Icons/truck.svg"
+    },
+    'account_balance': {
+        text: "Account Balance",
+        field: "TSACreditLimit",
+        measure_unit: "Points",
+        svg:"https://storage.pepperi.com/General/Icons/balance.svg"
+    },
+    'active-order':
+    {
+        name: "My Current Order",
+        table: [{
+            text: "Sub Total",
+            field: "SubTotal"
+        }, {
+            text: "Total Quantity",
+            field: "QuantitiesTotal"
+        }, {
+            text: "Total Sum",
+            field: "GrandTotal"
+        }]
+    }
+    ,
+    'submitted_orders': {
+        name: "Last Orders",
+        statuses: ["2"],
+        table: ["ActionDateTime", "InternalID"]
+    }
+}
+var Brands = [
+    {
+        bigImage: true,
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop2%5C%22,%5C%22Value%5C%22:%5C%22Face%20Serums%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22023bad44-a6d3-435b-928d-bab0a41b4dc6%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/8.jpg',
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop1%5C%22,%5C%22Value%5C%22:%5C%22Hair%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22696718b2-3431-4aad-b23a-8a6d7c824ef0%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/7.jpg',
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop2%5C%22,%5C%22Value%5C%22:%5C%22Dry%20Shampoo%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22696718b2-3431-4aad-b23a-8a6d7c824ef0%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/16.jpg',
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop2%5C%22,%5C%22Value%5C%22:%5C%22Co%20Wash%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22696718b2-3431-4aad-b23a-8a6d7c824ef0%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/17.jpg',
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop1%5C%22,%5C%22Value%5C%22:%5C%22Treatments%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22023bad44-a6d3-435b-928d-bab0a41b4dc6%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/18.jpg',
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop1%5C%22,%5C%22Value%5C%22:%5C%22Treatments%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22023bad44-a6d3-435b-928d-bab0a41b4dc6%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/19.jpg',
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop1%5C%22,%5C%22Value%5C%22:%5C%22Face%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%227142a6f9-af48-4dba-a30d-85d89b2ed083%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/23.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%223da50a6c-8a60-4b9c-93f6-a0c83de35072%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/6.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop2%5C%22,%5C%22Value%5C%22:%5C%22New%20Arrivals%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22696718b2-3431-4aad-b23a-8a6d7c824ef0%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/22.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22023bad44-a6d3-435b-928d-bab0a41b4dc6%5C%22%7D%22',
+        img: 'https://storage.pepperi.com/Beauty_demo/10.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop2%5C%22,%5C%22Value%5C%22:%5C%22New%20Arrivals%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%22696718b2-3431-4aad-b23a-8a6d7c824ef0%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/11.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%228cf1f19b-9815-40ea-becf-6eced87d910e%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/12.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22fcef195d-fcdd-4ce6-b6e7-f506bc92c46c%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/13.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop1%5C%22,%5C%22Value%5C%22:%5C%22Face%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%227142a6f9-af48-4dba-a30d-85d89b2ed083%5C%5C%5C%22%7D%5C%22%7D%22&amp;TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/21.jpg',
+
+    },
+    {
+        link: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22DynamicFilter%5C%22:%5C%22Item.Prop1%5C%22,%5C%22Value%5C%22:%5C%22Masks%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%222a7c613e-1e67-4497-af60-ae8a6633487a%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0',
+        img: 'https://storage.pepperi.com/Beauty_demo/15.jpg',
+
+    }]
+//Promotions block
+var Promotions = [
+    {
+        title: "Top 10 Sellers",
+        buttonText: "Shop Now",
+        link: 'Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22e3c559be-8581-4313-9095-cb5c113c3564%5C%22%7D%22&TopPadding=0&SmartSearch=%5B%7B%22ApiName%22:%22ItemMainCategory%22,%22ComparisonType%22:%22Values%22,%22Values%22:%5B%22Paul%20Pitchell%22%5D%7D%5D',
+        image: 'https://storage.pepperi.com/Beauty_demo/1.jpg'
+    },
+    {
+        title: "Recommendations for You",
+        buttonText: "Shop Now",
+        link: 'Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%223da50a6c-8a60-4b9c-93f6-a0c83de35072%5C%22%7D%22&TopPadding=0',
+        image: 'https://storage.pepperi.com/Beauty_demo/2.jpg'
+    },
+    {
+        title: "Sign up for Newsletter",
+        buttonText: "Shop Now",
+        link: 'Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22e3c559be-8581-4313-9095-cb5c113c3564%5C%22%7D%22&TopPadding=0&SmartSearch=%5B%7B%22ApiName%22:%22ItemMainCategory%22,%22ComparisonType%22:%22Values%22,%22Values%22:%5B%22Beauty%20%26%20Make%20Up%22%5D%7D%5D',
+        image: 'https://storage.pepperi.com/Beauty_demo/5.jpg'
+    },
+    {
+        title: "Virtual Try on",
+        buttonText: "Shop Now",
+        link: 'Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22e3c559be-8581-4313-9095-cb5c113c3564%5C%22%7D%22&TopPadding=0&SmartSearch=%5B%7B%22ApiName%22:%22ItemMainCategory%22,%22ComparisonType%22:%22Values%22,%22Values%22:%5B%22Beauty%20%26%20Make%20Up%22%5D%7D%5D',
+        image: 'https://storage.pepperi.com/Beauty_demo/5.jpg'
+    }
+]
+
+var CaruselData = [{
+
+    title: 'View Seraphin Spring 2021 Collection',
+    imageURL: 'https://ogieyewear-my.sharepoint.com/:i:/p/katja_beier/ETfFC9vOQZRFmfpLtAFWZKEBuWrj96V9AiQwXW_9CMg6KA',
+    buttonText: 'View',
+    time: 5000,
+    deepLink: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22defaef5e-e6d3-4b1e-b53c-2fa41f700168%5C%22%7D%22&ViewType=%7B%22Key%22:%22OrderCenterView3%22,%22Value%22:%22Medium%22%7D&TopPadding=0&SearchAll=false'
+},
+{
+
+    title: "The Sky's the Limit with OGI Eyewear",
+    imageURL: 'https://ogieyewear-my.sharepoint.com/:i:/p/katja_beier/ESmmo6OVFaxAmFhAq9JtFsoBl7WDkQVBDooPtsF6wdM4Bw',
+    buttonText: 'View',
+    time: 5000,
+    deepLink: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22defaef5e-e6d3-4b1e-b53c-2fa41f700168%5C%22%7D%22&ViewType=%7B%22Key%22:%22OrderCenterView3%22,%22Value%22:%22Medium%22%7D&TopPadding=0&SearchAll=false'
+},
+{
+
+    title: 'New Seraphin Shimmer Capsule Collection',
+    imageURL: 'https://ogieyewear-my.sharepoint.com/:i:/p/katja_beier/EfN9UAYTBt5JudTKGciON3QBe-dcFOEAYw2PIJrg9KLYHg',
+    buttonText: 'View',
+    time: 5000,
+    deepLink: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22defaef5e-e6d3-4b1e-b53c-2fa41f700168%5C%22%7D%22&ViewType=%7B%22Key%22:%22OrderCenterView3%22,%22Value%22:%22Medium%22%7D&TopPadding=0&SearchAll=false'
+},
+{
+
+    title: 'Discover Red Rose Fresh Styles',
+    imageURL: 'https://ogieyewear-my.sharepoint.com/:i:/p/katja_beier/ESghZ7hAfrhLinwS7nIYbPoBhNt__teCWa0wyf6ZLTeMoQ',
+    buttonText: 'View',
+    time: 5000,
+    deepLink: '/Transactions/scope_items/{{UUID}}?CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22defaef5e-e6d3-4b1e-b53c-2fa41f700168%5C%22%7D%22&ViewType=%7B%22Key%22:%22OrderCenterView3%22,%22Value%22:%22Medium%22%7D&TopPadding=0&SearchAll=false'
+},
+];
---
+++ OGI/ogi_body.js
@@ -0,0 +1,181 @@
+/*******************************************Ver: 19-06-2019 - fix to open orders list***************************************************/
+//                                  Pepperi plugin interface
+/**********************************************************************************************/
+//  1. The namespace of the header must be "customHomepage".
+//  3. The header must implement those functions:
+//     * initPlugin - This method is to get the option object with the declared interface:
+//          - JsURLs: the path of the script files.
+//          - cssURLs: the path of the css files.
+//          - more...
+//     * onPluginLoad(context) - This method is called when the plugin is ready for use (external files are loaded).
+//          - params: context object .
+//     * navigateTo - (Optional) The event for the navigation the data need to be like this:
+//          - detail: {
+//              path: 'HomePage' - (the path is the url that you want).
+//          }
+//     * logout - (Optional) The event for the logout the name of the event have to be 'logout':
+//
+/**********************************************************************************************/
+var customHomepage = {};
+(function () {
+  this.context;
+  this.accountUUID;
+  this.catalogs = [];
+  this.accounts = [];
+  this.jsonFilePath = 'https://pepperihomepage.github.io/Public/Beauty/config_body.js'
+  this.jsonModuleChatFilePath = 'https://storage.pepperi.com/PreSales/beauty_demo/chat.js'
+  this.promotionsJsonPath = 'https://pepperihomepage.github.io/Public/promotions/body_promotions.js'
+  this.brandscssPath = "https://pepperihomepage.github.io/Public/brands/body_brands.css";
+  this.brandsJsonPath = 'https://pepperihomepage.github.io/Public/brands/body_brands.js'
+  this.brandscssPath = "https://pepperihomepage.github.io/Public/brands/body_brands.css";
+  this.carousalJsonPath = 'https://pepperihomepage.github.io/Public/carousal/body_carousel.js'
+  this.carousalcssPath = "https://pepperihomepage.github.io/Public/carousal/body_carousal.css";
+  this.freeShippingJsonPath = 'https://pepperihomepage.github.io/Public/sidebar/free-shipping/body_sidebar_free_shipping.js'
+  this.freeShippingCssPath = 'https://pepperihomepage.github.io/Public/sidebar/free-shipping/body_sidebar_free_shipping.css'
+  this.accountBalanceJsonPath = 'https://pepperihomepage.github.io/Public/sidebar/account-balance/body_sidebar-account_balance.js'
+  this.accountBalanceCssPath = 'https://pepperihomepage.github.io/Public/sidebar/account-balance/body_sidebar-account_balance.css'
+  this.activeOrderJsonPath = 'https://pepperihomepage.github.io/Public/sidebar/active-order/body_sidebar_active-order.js'
+  this.activeOrderCssPath = 'https://pepperihomepage.github.io/Public/sidebar/active-order/body_sidebar-active-order.css'
+  this.submitedOrderJsonPath = 'https://pepperihomepage.github.io/Public/sidebar/submitted-orders/body_sidebar-submitted-orders.js'
+  this.submitedOrderCssPath = 'https://pepperihomepage.github.io/Public/sidebar/submitted-orders/body_sidebar-submitted-orders.css'
+  this.accountDropdownJsonPath = 'https://pepperihomepage.github.io/Public/sidebar/account-dropdown/body_sidebar_store-selector.js'
+  this.accountDropdownCssPath = 'https://pepperihomepage.github.io/Public/sidebar/account-dropdown/body_sidebar_store-selector.css'
+  this.navigationJsonPath = 'https://pepperihomepage.github.io/Public/navigation/body_navigation.js'
+  this.jsListenersJsonPath = 'https://pepperihomepage.github.io/Public/jsListeners/jsListeners.js'
+  this.helperJsonPath = 'https://pepperihomepage.github.io/Public/helper/body_helper.js'
+  this.customHelperJsonPath = 'https://pepperihomepage.github.io/Public/helper/customFunction.js'
+  this.isMultiAccount = true
+  this.cssFilePath = "";
+  this.transactionFields = []
+  this.transactionsHistoryFields = []
+  this.setHtml = function () {
+    var str = `            
+            <main class="wrapper">
+            <section id="carousal-content">
+            </section>
+            <aside id="sidebar">
+              <div id="response-menu" class="response-menu">
+                <button onclick="customFunction.openCloseMenu();" class="regular-button" id="btn">Open menu</button>
+              </div>
+          
+              <div id="sidebar-sm" class="sidebar-menu">
+                <div id="store-selector" style="display:none">                  
+                </div>
+          
+                <!--<hr>-->
+                 
+                <div id="free_shipping" style="display:none">
+                </div>
+          
+                <div id="account_balance" style="display:none">
+                </div>
+          
+                <hr id ="store-selector-hr" style="display:none">
+          
+                <div id="active-order" style="display:none">
+                  
+                </div>
+          
+                <hr class="sidebar-gap">
+          
+                <div id="submitted_orders" style="display:none">
+                  
+                </div>
+                <div id="overlay"></div>
+              </div>
+          
+          
+            </aside>
+          
+            <div id="categories">
+              <div id="brands">
+          
+              </div>
+              <div id="promotions">
+                
+              </div>
+            </div>
+          </main>
+    `;
+    return str;
+  };
+  this.initPlugin = function () {
+    
+    var options = {
+      JsURLs: [this.jsonFilePath,
+        this.jsonModuleChatFilePath,
+        this.promotionsJsonPath,
+        this.brandsJsonPath,
+        this.carousalJsonPath,
+        this.freeShippingJsonPath,
+        this.accountBalanceJsonPath,
+        this.activeOrderJsonPath,
+        this.submitedOrderJsonPath,
+        this.accountDropdownJsonPath,
+        this.navigationJsonPath,
+        this.jsListenersJsonPath,
+        this.helperJsonPath,
+        this.customHelperJsonPath
+      ],
+      cssURLs: [this.cssFilePath,
+        this.carousalcssPath,
+        this.brandscssPath,
+        this.freeShippingCssPath,
+        this.accountBalanceCssPath,
+        this.submitedOrderCssPath,
+        this.activeOrderCssPath,
+        this.accountDropdownCssPath
+      ],
+    };
+    
+    console.log("initPlugin body",options)
+    return options;
+  };
+  this.onPluginLoad = function (context) {
+    console.log("onPluginLoad body")
+    this.context = context;
+    var data = JSON.parse(context.pluginData);
+    if (data && this.isMultiAccount) {
+      this.accountUUID = this.getSessionStorage("accountUUID") || "";
+    } else if (data) {
+      this.accountUUID = data.accountUUID
+      customFunction.setSessionStorage("accountUUID", data.accountUUID)
+    }
+    customFunction.getCatalogs('customHomepage');
+  };
+
+  // TODO: start
+
+  //end  
+  this.buildHTML = function () {
+    //try to remove ifelse, settimeout also remove
+    
+    customFunction.closeAllMenusListener();
+    customFunction.carousel("carousal-content", CaruselData)
+    customFunction.drawImagesBlocks("brands", Brands)
+    customFunction.drawPromotions("promotions", Promotions)
+    customFunction.getAccounts('customHomepage.findTransactionForSelectedAccount');
+  };
+
+
+  customHomepage.getSessionStorage = function (paramName) {
+    return sessionStorage.getItem(paramName);
+  };
+  this.findTransactionForSelectedAccount = function (uuid) {
+    console.log("uuid -----> ", uuid)
+    this.accountUUID = uuid;
+    customFunction.setSessionStorage("accountUUID", uuid);
+    if (blocks_config.free_shipping) {
+      customFunction.freeShipping(uuid, blocks_config.free_shipping, "free_shipping")
+    }
+    if (blocks_config.account_balance) {
+      customFunction.accountBalance(uuid, blocks_config.account_balance, "account_balance")
+    }
+    if (blocks_config["active-order"]) {
+      customFunction.activeOrder(customFunction.transactionName, blocks_config["active-order"].table, uuid, "active-order")
+    }
+    if (blocks_config["submitted_orders"]) {
+      customFunction.submitedOrders(customFunction.transactionName, blocks_config["submitted_orders"].table, uuid, "submitted_orders")
+    }
+  }
+}.apply(customHomepage));
