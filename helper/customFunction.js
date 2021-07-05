a53ee9767976c1f7aa2f4ef4f224a706891df7bf
Kiril Chernysh <kiril.chernysh@gmail.com>
2021-07-05 17:21:04 +0300

    fixed custom function v2

--- helper/customFunction.js
+++ helper/customFunction.js
@@ -108,7 +108,7 @@ customFunction.openInNewTab = function (url) {
 
 customFunction.createNewOrder = function (inCatalog = null, in_transactionName = null, deepLink = null, skipSessionSaving, nameOfMainJs) {
   var name = eval("(" + nameOfMainJs + ")")
-  let catalogUUID = !inCatalog ? name.catalogs.find((el) => el.ExternalID === name.catalogName).UUID : name.catalogs.find((el) => el.ExternalID === inCatalog).UUID
+  let catalogUUID = !inCatalog ? customFunction.catalogs.find((el) => el.ExternalID === customFunction.catalogName).UUID : customFunction.catalogs.find((el) => el.ExternalID === inCatalog).UUID
   var bridgeObject = {
     references: {
       account: {
