aff0a533cec82ab7b2ad3140bbc13d9a42794dc1
Kiril Chernysh <kiril.chernysh@gmail.com>
2021-07-22 16:35:40 +0300

    added new photos for OGI

--- OGI/config_body.js
+++ OGI/config_body.js
@@ -41,16 +41,16 @@ var Brands = [
     },
     {
         link: '/transactions/scope_items/{{UUID}}?SearchString=&CurrentTab="%7B%5C"JsonFilter%5C":%5C"c8ab1f8d-4ebe-4f0e-9735-0e58b50504c3%5C",%5C"Parent%5C":%5C"%7B%5C%5C%5C"DynamicFilter%5C%5C%5C":%5C%5C%5C"Item.MainCategory%5C%5C%5C",%5C%5C%5C"Value%5C%5C%5C":%5C%5C%5C"Ogi%20Kids%5C%5C%5C"%7D%5C"%7D"&TopPadding=0&SearchAll=false',
-        img: 'https://pepperihomepage.github.io/Public/OGI/img/brand_OGI%20Kids.jpg',
+        img: 'https://pepperihomepage.github.io/Public/OGI/img/brand_OGI%20Kids.png',
     },
     {
         link: '/transactions/scope_items/{{UUID}}?SearchString=&CurrentTab="%7B%5C"JsonFilter%5C":%5C"c8ab1f8d-4ebe-4f0e-9735-0e58b50504c3%5C",%5C"Parent%5C":%5C"%7B%5C%5C%5C"DynamicFilter%5C%5C%5C":%5C%5C%5C"Item.MainCategory%5C%5C%5C",%5C%5C%5C"Value%5C%5C%5C":%5C%5C%5C"Scojo%5C%5C%5C"%7D%5C"%7D"&TopPadding=0&SearchAll=false',
-        img: 'https://pepperihomepage.github.io/Public/OGI/img/brand_SCOJO.jpg',
+        img: 'https://pepperihomepage.github.io/Public/OGI/img/brand_SCOJO.png',
     }]
 //Promotions block
 var Promotions = [
     {
-        title: "Top 10 Sellers",
+        title: "Top Sellers",
         link: '/transactions/scope_items/{{UUID}}?SearchString=&CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%22a58db397-4b63-4131-80fa-d8257aa33b34%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%225e41726a-2442-46e9-9825-c092569dc7a9%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0&SearchAll=false',
         image: 'https://pepperihomepage.github.io/Public/OGI/img/1.Top%20sellers_promotions.jpg'
     },
@@ -75,7 +75,7 @@ var Promotions = [
 
 var CaruselData = [{
     title: 'View Seraphin Spring 2021 Collection',
-    imageURL: 'https://pepperihomepage.github.io/Public/OGI/img/CarouselSeraphin.jpg',
+    imageURL: 'https://pepperihomepage.github.io/Public/OGI/img/CarouselSeraphin.png',
     imageURLIpad: 'https://pepperihomepage.github.io/Public/OGI/img/ipadbanners-seraphin.png',
     imageURLIphone: 'https://pepperihomepage.github.io/Public/OGI/img/iphone-seraphin.png',
     buttonText: 'See the Collection',
@@ -84,7 +84,7 @@ var CaruselData = [{
 },
 {
     title: "The Sky's the Limit with OGI Eyewear",
-    imageURL: 'https://pepperihomepage.github.io/Public/OGI/img/CarouselOGI.jpg',
+    imageURL: 'https://pepperihomepage.github.io/Public/OGI/img/CarouselOGI.png',
     imageURLIpad: 'https://pepperihomepage.github.io/Public/OGI/img/ipadbanners-ogi.png',
     imageURLIphone: 'https://pepperihomepage.github.io/Public/OGI/img/iphone-ogi.png',
     buttonText: 'See the Collection',
@@ -93,16 +93,16 @@ var CaruselData = [{
 },
 {
     title: 'New Seraphin Shimmer Capsule Collection',
-    imageURL: 'https://pepperihomepage.github.io/Public/OGI/img/CarouselShimmer.jpg',
+    imageURL: 'https://pepperihomepage.github.io/Public/OGI/img/CarouselShimmer.png',
     imageURLIpad: 'https://pepperihomepage.github.io/Public/OGI/img/ipadbanners-shimmer.png',
     imageURLIphone: 'https://pepperihomepage.github.io/Public/OGI/img/iphone-shimmer.png',
-    buttonText: 'See the Collection',
+    buttonText: 'View Timeless Seraphin Styles',
     time: 5000,
     deepLink: '/transactions/scope_items/{{UUID}}?SearchString=&CurrentTab="%7B%5C"DynamicFilter%5C":%5C"Item.TSASeries%5C",%5C"Value%5C":%5C"Shimmer%5C",%5C"Parent%5C":%5C"%7B%5C%5C%5C"DynamicFilter%5C%5C%5C":%5C%5C%5C"Item.MainCategory%5C%5C%5C",%5C%5C%5C"Value%5C%5C%5C":%5C%5C%5C"Seraphin%5C%5C%5C"%7D%5C"%7D"&TopPadding=0&SearchAll=false'
 },
 {
     title: 'Discover Red Rose Fresh Styles',
-    imageURL: 'https://pepperihomepage.github.io/Public/OGI/img/CarouselRR.jpg',
+    imageURL: 'https://pepperihomepage.github.io/Public/OGI/img/CarouselRR.png',
     imageURLIpad: 'https://pepperihomepage.github.io/Public/OGI/img/ipadbanners-redrose.png',
     imageURLIphone: 'https://pepperihomepage.github.io/Public/OGI/img/iphone-redrose.png',
     buttonText: 'See the Collection',
