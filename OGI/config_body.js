e060a2d3126d4f51ff3fc82fb4179f52228969d7
SenchukD <99643455+SenchukD@users.noreply.github.com>
2022-04-28 10:32:13 +0300

    Update config_body.js

--- OGI/config_body.js
+++ OGI/config_body.js
@@ -74,20 +74,20 @@ var Promotions = [
 ]
 
 var CaruselData = [{
-    title: "Save now with our April Promotion",
+    title: "Sky's the Limit with OGI Eyewear",
     imageURL: 'https://pepperihomepage.github.io/Public/OGI/img/sky_limit_new_ogi.jpg',
     imageURLIpad: 'https://pepperihomepage.github.io/Public/OGI/img/ipadbanners-ogi.png',
     imageURLIphone: 'https://pepperihomepage.github.io/Public/OGI/img/iphone-ogi.png',
-    buttonText: 'Shop Now',
+    buttonText: 'See the Collection',
     time: 5000,
     deepLink: '/transactions/scope_items/{{UUID}}?SearchString=&CurrentTab="%7B%5C"JsonFilter%5C":%5C"c8ab1f8d-4ebe-4f0e-9735-0e58b50504c3%5C",%5C"Parent%5C":%5C"%7B%5C%5C%5C"DynamicFilter%5C%5C%5C":%5C%5C%5C"Item.MainCategory%5C%5C%5C",%5C%5C%5C"Value%5C%5C%5C":%5C%5C%5C"Ogi%5C%5C%5C"%7D%5C"%7D"&TopPadding=0&SearchAll=false'
 },
     {
-    title: "",
+    title: "Save now with our April Promotion",
     imageURL: 'https://pepperihomepage.github.io/Public/OGI/img/april_promotion_ogi.jpg',
     imageURLIpad: 'https://pepperihomepage.github.io/Public/OGI/img/april_promotion_ogi.jpg',
     imageURLIphone: 'https://pepperihomepage.github.io/Public/OGI/img/april_promotion_ogi.jpg',
-    buttonText: '',
+    buttonText: 'Shop Now',
     time: 5000,
     deepLink: '/transactions/scope_items/{{UUID}}?SearchString=&CurrentTab="%7B%5C"JsonFilter%5C":%5C"c8ab1f8d-4ebe-4f0e-9735-0e58b50504c3%5C",%5C"Parent%5C":%5C"%7B%5C%5C%5C"DynamicFilter%5C%5C%5C":%5C%5C%5C"Item.MainCategory%5C%5C%5C",%5C%5C%5C"Value%5C%5C%5C":%5C%5C%5C"Ogi%5C%5C%5C"%7D%5C"%7D"&TopPadding=0&SearchAll=false'
 },
@@ -226,4 +226,4 @@ var Footer = {
             Link: "https://www.ogieyewear.com/ogi-ceo"
         }
     ]
-}
+}   
