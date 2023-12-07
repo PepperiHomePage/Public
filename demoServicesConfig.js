7023a7165acfd516313b4a7f0458c7e23b082467
SenchukD <99643455+SenchukD@users.noreply.github.com>
2023-12-07 22:46:08 +0200

    Add files via upload

---
+++ demoServicesConfig.js
@@ -0,0 +1,70 @@
+const LeftMenu = [
+    {
+      title: "Accounts",
+      deepLink: '/list/accounts'
+    },
+    {
+      title: "Activities",
+      deepLink: 'list/all_activities'
+    },
+    {
+      title: "Users",
+      deepLink: 'list/users'
+    },
+    {
+      title: "Contacts",
+      deepLink: 'list/contacts'
+    },
+    {
+      title: "Items",
+      deepLink: 'list/items'
+    },
+    {
+      title: "Offline Order Splitter",
+      deepLink: 'Transactions/scope_items/{{UUID}}'
+    },
+    {
+      title: "Bulk Order Upload",
+      deepLink: 'activities/details/{{UUID}}'
+    },
+    {
+      title: "Kangaroo, Order Details, Notes",
+      deepLink: 'Transactions/scope_items/{{UUID}}'
+    },
+    {
+      title: "Return Without Validations",
+      deepLink: 'Transactions/scope_items/{{UUID}}'
+    },
+    {
+      title: "Return Based On UDT",
+      deepLink: 'Transactions/scope_items/{{UUID}}'
+    },
+    {
+      title: "Sales Order Template 1",
+      deepLink: 'Transactions/scope_items/{{UUID}}'
+    },
+    {
+      title: "B2B Order",
+      deepLink: 'Transactions/scope_items/{{UUID}}'
+    },
+    {
+      title: "Script Order",
+      deepLink: 'Transactions/scope_items/{{UUID}}'
+    },
+    {
+      title: "Frame Board Management",
+      deepLink: 'Transactions/scope_items/{{UUID}}'
+    },
+    {
+      title: "Ledger",
+      deepLink: 'activities/details/{{UUID}}'
+    },
+    {
+      title: "Account Insights Page",
+      deepLink: 'account_insights'
+    },
+    {
+      title: "Insights Page",
+      deepLink: 'insights'
+    },
+  ];
